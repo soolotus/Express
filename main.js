7beef09ad578eac52b1de009791a1c3bdf6dcde9
Soo Yeon Seong <58795942+soolotus@users.noreply.github.com>
2024-02-22 00:21:17 -0700

    refactory

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-const template = require('./lib/template.js');
+
 const fs = require("fs")
-const path = require('path');
-const sanitizeHtml = require('sanitize-html');
+
+
 const qs = require('querystring');
 const bodyParser = require('body-parser')
 const compression = require('compression')
-
+const topicRouter = require('./routes/topic')
+const indexRouter = require('./routes/index')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -20,155 +21,12 @@ fs.readdir('./data', (err, filelist)=>{
   //다음에 실행되어야 할 미들웨어를 실함.
 })
 })
+app.use('/', indexRouter)
+//  /topic으로 시작하는 주소들에게 topicRouter라는 이름의 미들웨어를 적용하겠다.
+app.use('/topic',topicRouter)
+
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(req.list);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}
-      <img src="/images/hello.jpg" style="width:300px; display:block; margin-top: 10px;" />
-      `,
-      `<a href="/create">create</a>`
-    )
-    res.send(html)
-  
-})
-
-app.get('/page/:pageId', (req, res, next)=> {
-  console.log(req.list)
- 
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      if (err) {
-        next(err)
-
-      } else {
-      
-       var title = req.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-        var list = template.list(req.list);
-        var html = template.HTML(sanitizedTitle, list,
-          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-          ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-        );
-        res.send(html);
-      }
-      
-}
-)})
-
-app.get('/create', (req, res)=>{
-
-    var title = 'WEB - create';
-    var list = template.list(req.list);
-    var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    res.send(html);
-  
-})
-
-app.post('/create_process', (req, res)=>{
-  console.log(req.list)
- 
-  /*
-  var body = '';
-  req.on('data', function(data){
-      body = body + data;
-  });
-  req.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        res.writeHead(302, {Location: `/page/${title}`});
-        res.end();
-      })
-  });
-  */
-
- var post =  req.body
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        res.writeHead(302, {Location: `/page/${title}`});
-        res.end();
-      })
-})
-
-app.get('/update/:updateId', (req, res)=>{
-  
-    var filteredId = path.parse(req.params.updateId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = req.params.updateId
-      var list = template.list(req.list);
-      var html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      res.send(html);
-    });
-  
-})
-
-app.post('/update_process', (req, res)=>{
-  
-          var post = req.body;
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              
-              res.redirect(`/page/${title}`)
-            })
-          });
-      
-
-})
-
-app.post('/delete_process', (req, res)=>{
-  
-          var post = req.body ;
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            res.redirect('/')
-          })
-      
-})
-
-
 
 app.use((req, res, next)=>{
   res.status(404).send("Sorry cannot find that")
